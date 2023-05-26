ae5ecfe8c0dcc316ccb472f3a716d4ee45f555ce
Moaaz Khairy <moazkhairy@example.com>
2023-05-26 19:56:12 +0300

    initial commit

---
+++ src/main/java/com/example/CIT611_project/Cit611ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.CIT611_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cit611ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cit611ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CIT611_project/Cit611ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CIT611_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cit611ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
